0d12feae1d5ed31bb1a18a0a25d9fa412a2926fd
Dmitrii Priimak <priimak69@inbox.ru>
2023-07-11 20:17:21 +0700

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,12 @@
 ﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
 // Выполнить с помощью рекурсии.
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Input natural namber: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void NaturNam (int n)
+{
+if (n > 1) NaturNam (n-1);
+Console.Write(n+" ");
+}
+NaturNam(n);
