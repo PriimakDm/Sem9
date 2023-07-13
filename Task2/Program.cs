aac6b6e9d8a90d9a448535f1b42d2b68ea0c782c
Dmitrii Priimak <priimak69@inbox.ru>
2023-07-13 15:29:47 +0700

    Task3

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Задайте значения m и n. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от m до n.
 
 Console.WriteLine("Input first natural namber: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input another natural namber: ");
 int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input another natural namber: ");
+int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
 
 int SumNatElements(int m, int n)
@@ -12,7 +12,7 @@ int SumNatElements(int m, int n)
     else if (m == 0) return (n * (n + 1)) / 2;
     else if (m == n) return m;
     else if (n < m) return n + SumNatElements(m, n + 1);
-    else return n + SumNatElements(m, n - 1);
+    else if (m < n) return n + SumNatElements(m, n - 1);
     
 }
 Console.WriteLine($"Sum of elements: {SumNatElements(m, n)}");
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,23 @@
 ﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-Console.WriteLine("Hello, World!");
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Input first namber: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second namber: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int FunAkk(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return FunAkk(n - 1, 1);
+    else
+      return FunAkk(n - 1, FunAkk(n, m - 1));
+}
+
+Console.WriteLine($"Akkermann function A({m},{n})={FunAkk(m, n)}");
+
